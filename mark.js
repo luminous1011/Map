99e397daf8e6915bdc019b16e87c2c205f805c4b
panjinshan <panjinshan@iauto.com>
2021-05-18 18:07:38 +0800

    'map'

---
+++ mark.js
@@ -0,0 +1,452 @@
+//安徽
+const anhui_01 = [//芜湖王稼祥纪念园
+    {
+        message: '芜湖王稼祥纪念园',
+        value: [118.362403, 31.342248]
+    },
+];
+const anhui_02 = [//芜湖李家发纪念馆
+    {
+        message: '芜湖李家发纪念馆',
+        value: [118.257851, 30.991174]
+    }
+]
+const anhui_03 = [//刘邓大军大别山前方指挥部
+    {
+        message: '刘邓大军大别山前方指挥部',
+        value: [115.765601, 31.169081]
+    }
+]
+const anhui_04 = [//渡江战役纪念馆
+    {
+        message: '渡江战役纪念馆',
+        value: [117.330014, 31.709653]
+    }
+]
+const anhui_05 = [//新四军军部旧址
+    {
+        message: '新四军军部旧址',
+        value: [118.227287, 30.651284]
+    }
+]
+//福建
+const fujian_01 = [//古田会议
+    {
+        message: '古田会议',
+        value: [116.826084, 25.216883]
+    }
+]
+//甘肃
+const gansu_01 = [//陕甘边区苏维埃政府旧址
+    {
+        message: '陕甘边区苏维埃政府旧址',
+        value: [108.343686, 36.398352]
+    }
+]
+//广东
+const guangdong_01 = [//广州起义
+    {
+        message: '广州起义',
+        value: [113.264263, 23.124261]
+    }
+]
+const guangdong_02 = [//中共三大会址
+    {
+        message: '中共三大会址',
+        value: [113.296077, 23.118943]
+    }
+]
+//贵州
+const guizhou_01 = [//遵义会议
+    {
+        message: '遵义会议',
+        value: [106.920726, 27.688412]
+    }
+]
+//海南
+const hainan_01 = [//红色娘子军纪念园
+    {
+        message: '红色娘子军纪念园',
+        value: [110.431155, 19.210252]
+    }
+]
+//河北
+const heibei_01 = [//狼牙山五勇士纪念馆
+    {
+        message: '狼牙山五勇士纪念馆',
+        value: [115.173582, 39.140006]
+    }
+]
+const heibei_02 = [//雄安新区白洋淀博物馆
+    {
+        message: '雄安新区白洋淀博物馆',
+        value: [115.966033, 38.912223]
+    }
+]
+//河南
+const henan_01 = [//郑州二七罢工纪念塔
+    {
+        message: '郑州二七罢工纪念塔',
+        value: [113.666518, 34.752269]
+    }
+]
+//黑龙江
+const heilongjiang_01 = [//东北抗联博物馆
+    {
+        message: '东北抗联博物馆',
+        value: [126.643006,45.766318]
+    }
+]
+//湖北
+const hubei_01 = [//八七会议,枪杆子里出政权
+    {
+        message: '八七会议,枪杆子里出政权',
+        value: [114.299797, 30.586586]
+    }
+]
+const hubei_02 = [//辛亥革命武昌起义军政府旧址
+    {
+        message: '辛亥革命武昌起义军政府旧址',
+        value: [114.306333, 30.542024]
+    }
+]
+//湖南
+const hunan_01 = [//毛泽东纪念馆
+    {
+        message: '毛泽东纪念馆',
+        value: [112.491843, 27.905372]
+    }
+]
+//吉林
+const jilin_01 = [//四平战役纪念馆
+    {
+        message: '四平战役纪念馆',
+        value: [124.360225, 43.174937]
+    }
+]
+//江苏
+const jiangsu_01 = [//新四军太湖游击队纪念馆
+    {
+        message: '新四军太湖游击队纪念馆',
+        value: [120.313571, 31.272102]
+    }
+]
+const jiangsu_02 = [//无锡张闻天故居
+    {
+        message: '无锡张闻天故居',
+        value: [120.297804, 31.569451]
+    }
+]
+const jiangsu_03 = [//无锡江阴天华军体博物馆
+    {
+        message: '无锡江阴天华军体博物馆',
+        value: [120.4573, 31.784952]
+    }
+]
+const jiangsu_04 = [//茅山新四军纪念馆
+    {
+        message: '茅山新四军纪念馆',
+        value: [119.300172, 31.787113]
+    }
+]
+//江西
+const jiangxi_01 = [//井冈山革命根据地
+    {
+        message: '井冈山革命根据地',
+        value: [114.290346, 26.749109]
+    }
+]
+const jiangxi_02 = [//三湾改编纪念馆
+    {
+        message: '三湾改编纪念馆',
+        value: [113.973062, 26.8431]
+    }
+]
+//辽宁
+const liaoning_01 = [//辽沈战役纪念馆
+    {
+        message: '辽沈战役纪念馆',
+        value: [121.148175, 41.129367]
+    }
+]
+//青海
+const qinghai_01 = [//西宁市中国工农红军西路军纪念馆
+    {
+        message: '西宁市中国工农红军西路军纪念馆',
+        value: [101.762835, 36.594321]
+    }
+]
+//山东
+const shandong_01 = [//山东省政府和八路军115师司令部旧址
+    {
+        message: '山东省政府和八路军115师司令部旧址',
+        value: [117.020725, 36.670201]
+    }
+]
+const shandong_02 = [//海军博物馆
+    {
+        message: '海军博物馆',
+        value: [120.327714, 36.054786]
+    }
+]
+//山西
+const shanxi_01 = [//山西武乡县八路军太行纪念馆
+    {
+        message: '山西武乡县八路军太行纪念馆',
+        value: [112.855476, 36.834098]
+    }
+]
+//陕西
+const SHANXI_01 = [//八路军先办事处纪念馆
+    {
+        message: '八路军先办事处纪念馆',
+        value: [108.954882, 34.272681]
+    }
+]
+const SHANXI_02 = [//西安事变兵谏亭
+    {
+        message: '西安事变兵谏亭',
+        value: [109.216037, 34.360787]
+    }
+]
+const SHANXI_03 = [//西安事变纪念馆
+    {
+        message: '西安事变纪念馆',
+        value: [108.967721, 34.257313]
+    }
+]
+const SHANXI_04 = [//山西武乡县八路军太行纪念馆
+    {
+        message: '山西武乡县八路军太行纪念馆',
+        value: [112.855476, 36.834098]
+    }
+]
+//四川
+const sichuan_01 = [//成都红军长征纪念馆
+    {
+        message: '成都红军长征纪念馆',
+        value: [103.125779, 30.308965]
+    }
+]
+//云南
+const yunnan_01 = [//云南陆军讲武堂
+    {
+        message: '云南陆军讲武堂',
+        value: [102.700647, 25.047875]
+    }
+]
+//浙江
+const zhejiang_01 = [//嘉兴南湖一大会址
+    {
+        message: '嘉兴南湖一大会址',
+        value: [120.76019, 30.755192]
+    }
+]
+const zhejiang_02 = [//新四军苏浙军区纪念馆
+    {
+        message: '新四军苏浙军区纪念馆',
+        value: [119.682661, 31.072287]
+    }
+]
+const zhejiang_03 = [//浙东革命根据地纪念馆
+    {
+        message: '浙东革命根据地纪念馆',
+        value: [121.081446, 29.883658]
+    }
+]
+const zhejiang_04 = [//嘉兴南湖一大会址
+    {
+        message: '嘉兴南湖一大会址',
+        value: [120.76019, 30.755192]
+    }
+]
+//北京
+const beijing_01 = [//天安门
+    {
+        message: '天安门',
+        value: [116.397451, 39.909187]
+    }
+]
+//重庆
+const chongqing_01 = [//红岩革命纪念馆
+    {
+        message: '红岩革命纪念馆',
+        value: [106.499275, 29.553944]
+    }
+]
+const chongqing_02 = [//重庆中共党组织诞生地
+    {
+        message: '重庆中共党组织诞生地',
+        value: [116.208851, 40.038713]
+    }
+]
+const chongqing_03 = [//红岩革命历史博物馆
+    {
+        message: '红岩革命历史博物馆',
+        value: [106.441869, 29.572018]
+    }
+]
+const chongqing_04 = [//歌乐山烈士陵园
+    {
+        message: '歌乐山烈士陵园',
+        value: [106.44234, 29.572187]
+    }
+]
+//上海
+const shanghai_01 = [//八路军驻沪办事处
+    {
+        message: '八路军驻沪办事处',
+        value: [121.46416, 31.224497]
+    }
+]
+const shanghai_02 = [//中共四大纪念馆
+    {
+        message: '中共四大纪念馆',
+        value: [121.485252, 31.254969]
+    }
+]
+const shanghai_03 = [//四行仓库抗战纪念馆
+    {
+        message: '四行仓库抗战纪念馆',
+        value: [121.47115, 31.240346]
+    }
+]
+const shanghai_04 = [//龙华烈士陵园
+    {
+        message: '龙华烈士陵园',
+        value: [121.449233, 31.175805]
+    }
+]
+const shanghai_05 = [//中共一大会址纪念馆
+    {
+        message: '中共一大会址纪念馆',
+        value: [121.475111, 31.220123]
+    }
+]
+const shanghai_06 = [//外滩历史纪念馆
+    {
+        message: '外滩历史纪念馆',
+        value: [121.49147, 31.242302]
+    }
+]
+const shanghai_07 = [//陈云纪念馆
+    {
+        message: '陈云纪念馆',
+        value: [121.043851, 31.007039]
+    }
+]
+const shanghai_08 = [//中共二大会址纪念馆
+    {
+        message: '中共二大会址纪念馆',
+        value: [121.466714, 31.224169]
+    }
+]
+const shanghai_09 = [//国歌纪念馆&黄浦码头
+    {
+        message: '国歌纪念馆&黄浦码头',
+        value: [121.515158, 31.25745]
+    }
+]
+//天津
+const tianjin_01 = [//平津战役
+    {
+        message: '平津战役',
+        value: [117.146938, 39.168988]
+    }
+]
+//广西
+const guangxi_01 = [//湘江战役纪念馆
+    {
+        message: '湘江战役纪念馆',
+        value: [110.949593, 25.910416]
+    }
+]
+const guangxi_02 = [//八路军桂林办事处纪念馆
+    {
+        message: '八路军桂林办事处纪念馆',
+        value: [110.299575, 25.287023]
+    }
+]
+const guangxi_03 = [//红八军军部旧址
+    {
+        message: '红八军军部旧址',
+        value: [106.963291, 23.230072]
+    }
+]
+//内蒙古
+const neimenggu_01 = [//乌兰夫纪念馆
+    {
+        message: '乌兰夫纪念馆',
+        value: [111.643582, 40.813935]
+    }
+]
+//宁夏
+const ningxia_01 = [//六盘山红军长征纪念馆
+    {
+        message: '六盘山红军长征纪念馆',
+        value: [106.206978, 35.668091]
+    }
+]
+//澳门
+const aomen_01 = [//澳门回归贺礼陈列馆
+    {
+        message: '澳门回归贺礼陈列馆',
+        value: [113.559246, 22.187115]
+    }
+]
+//西藏
+const xizang_01 = [//青藏铁路建设
+    {
+        message: '青藏铁路建设',
+        value: [116.245392, 39.91242]
+    }
+]
+//香港
+const xianggang_01 = [//香港会议展览中心
+    {
+        message: '香港会议展览中心',
+        value: [114.178055, 22.279865]
+    }
+]
+//新疆
+const xinjiang_01 = [//乌鲁木齐烈士陵园
+    {
+        message: '乌鲁木齐烈士陵园',
+        value: [87.596512, 43.734495]
+    }
+]
+const mark = new Map([
+    ['安徽', [anhui_01, anhui_02, anhui_03, anhui_04, anhui_05,]],
+    ['福建', [fujian_01]],
+    ['甘肃', [gansu_01]],
+    ['广东', [guangdong_01, guangdong_02]],
+    ['贵州', [guizhou_01]],
+    ['海南', [hainan_01]],
+    ['河北', [heibei_01, heibei_02]],
+    ['河南', [henan_01]],
+    ['黑龙江',[heilongjiang_01]],
+    ['湖北', [hubei_01, hubei_02]],
+    ['湖南', [hunan_01]],
+    ['吉林', [jilin_01]],
+    ['江苏', [jiangsu_01, jiangsu_02, jiangsu_03, jiangsu_04]],
+    ['江西', [jiangxi_01, jiangxi_02]],
+    ['辽宁', [liaoning_01]],
+    ['青海', [qinghai_01]],
+    ['山东', [shandong_01, shandong_02]],
+    ['山西', [shanxi_01]],
+    ['陕西', [SHANXI_01, SHANXI_02, SHANXI_03, SHANXI_04]],
+    ['四川', [sichuan_01]],
+    ['云南', [yunnan_01]],
+    ['浙江', [zhejiang_01, zhejiang_02, zhejiang_03, zhejiang_04]],
+    ['北京', [beijing_01]],
+    ['重庆', [chongqing_01, chongqing_02, chongqing_03,chongqing_04]],
+    ['上海', [shanghai_01, shanghai_02, shanghai_03, shanghai_04, shanghai_05, shanghai_06, shanghai_07, shanghai_08, shanghai_09,]],
+    ['天津', [tianjin_01]],
+    ['广西', [guangxi_01, guangxi_02, guangxi_03]],
+    ['内蒙古', [neimenggu_01]],
+    ['宁夏', [ningxia_01]],
+    ['澳门', [aomen_01]],
+    ['西藏', [xizang_01]],
+    ['香港', [xianggang_01]],
+    ['新疆', [xinjiang_01]]
+])
